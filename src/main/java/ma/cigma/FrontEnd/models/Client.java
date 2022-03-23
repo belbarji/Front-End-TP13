f2c999c160f04d5c0d2bdcfd15d62fc0f1ce61bd
El BARJI Bilal <elbarji.bilal@gmail.com>
2022-03-23 18:27:47 +0100

    TP13

---
+++ src/main/java/ma/cigma/FrontEnd/models/Client.java
@@ -0,0 +1,19 @@
+package ma.cigma.FrontEnd.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Client {
+    private long id;
+    private String name;
+    public Client() {
+    }
+    public Client(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
